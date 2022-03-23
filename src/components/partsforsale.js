c79f90ec3012410d721836e54b2adac8e1a6eb64
Daniel Schnoll <23421605+danielschnoll@users.noreply.github.com>
2022-03-22 22:50:30 -0400

    tweaked styles on parts and about

--- src/components/partsforsale.js
+++ src/components/partsforsale.js
@@ -1,7 +1,9 @@
 import React from "react";
 import HalfHero from "../containers/halfHero";
 import GridContainer from "../containers/gridContainer";
+import { Container } from "react-bootstrap";
 
+import './partsforsale.scss'
 
 class PartsForSale extends React.Component {
   constructor(props) {
@@ -15,6 +17,11 @@ class PartsForSale extends React.Component {
     return (
       <div>
         <HalfHero title="PARTS FOR SALE" image="/images/home/banner/img2.jpg" />
+        <Container style={{paddingTop:"20px"}}>
+          <h3 className="text-center">Everything currently in our inventory is listed for sale on this parts page. If it isn't pictured, we do not have it. 
+            Please <a href="/contact">contact us</a> if you are interested in purchasing parts. Include the Bin Name and Letter/Number in your inquiry. Thanks!
+          </h3>
+        </Container>
         <GridContainer data={this.state.data} />
       </div>
     );
