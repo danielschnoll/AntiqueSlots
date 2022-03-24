71325e30fa0a86a5b04268cf7029f4c1f8059837
Daniel Schnoll <23421605+danielschnoll@users.noreply.github.com>
2022-03-24 12:43:58 -0400

    styled the 404 page

--- src/containers/404.js
+++ src/containers/404.js
@@ -1,29 +1,31 @@
 import React from "react";
-import { Container, Row, Col, Button} from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 
 import './404.scss'
 
-class NotFound404 extends React.Component{
-    
-    render(){
-        return(
-            <Container style={{height: "100vh"}}>
-                <Row className="row-center text-center">
-                    <Col md={4}>
-                        <img src="/extraordinary.svg" alt="Antique Slot Machine Artwork" className="image404"/>
-                    </Col>
-                    <Col md={4}>
-                        <h1>{"404"}</h1>
-                        <p>Uh-oh... It looks like the page you're looking for ain't here, chief!</p>
-                        <Button variant="danger" href="/">Go Back Home</Button>
-                    </Col>
-                    <Col md={4}>
-                        <img src="/extraordinary.svg" alt="Antique Slot Machine Artwork" className="image404"/>
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }  
+class NotFound404 extends React.Component {
+
+  render() {
+    return (
+      <div class="bg">
+        <Container style={{ height: "100vh" }}>
+          <Row className="row-center text-center">
+            <Col md={4}>
+              <img src="/extraordinary.svg" alt="Antique Slot Machine Artwork" className="image404" />
+            </Col>
+            <Col md={4}>
+              <h1>{"404"}</h1>
+              <p>Uh-oh... It looks like the page you're looking for ain't here, chief!</p>
+              <Button variant="danger" href="/">Go Back Home</Button>
+            </Col>
+            <Col md={4}>
+              <img src="/extraordinary.svg" alt="Antique Slot Machine Artwork" className="image404" />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default NotFound404
