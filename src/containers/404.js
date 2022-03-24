819c027798d370824fad192059498baa721d07b3
Daniel Schnoll <23421605+danielschnoll@users.noreply.github.com>
2022-03-24 10:57:12 -0400

    svg logos

--- src/containers/404.js
+++ src/containers/404.js
@@ -10,7 +10,7 @@ class NotFound404 extends React.Component{
             <Container style={{height: "100vh"}}>
                 <Row className="row-center text-center">
                     <Col md={4}>
-                        <img src="/extraordinary.webp" alt="Antique Slot Machine Artwork" className="image404"/>
+                        <img src="/extraordinary.svg" alt="Antique Slot Machine Artwork" className="image404"/>
                     </Col>
                     <Col md={4}>
                         <h1>{"404"}</h1>
@@ -18,7 +18,7 @@ class NotFound404 extends React.Component{
                         <Button variant="danger" href="/">Go Back Home</Button>
                     </Col>
                     <Col md={4}>
-                        <img src="/extraordinary.webp" alt="Antique Slot Machine Artwork" className="image404"/>
+                        <img src="/extraordinary.svg" alt="Antique Slot Machine Artwork" className="image404"/>
                     </Col>
                 </Row>
             </Container>
